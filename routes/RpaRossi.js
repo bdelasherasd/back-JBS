1fd6b6e8052bcd77548c3abba1b65997531f81ed
osboxes.org <bdelasheras@outlook.com>
2025-04-14 14:36:50 -0400

    rpa00

--- routes/RpaRossi.js
+++ routes/RpaRossi.js
@@ -160,12 +160,133 @@ var reprograma = function (taskdata, idTask) {
   global.tjobs.push({ id: idTask, job: job });
 };
 
+router.get("/procesaAhora/:referencia", cors(), async function (req, res) {
+  procesaAgenda(req, res, { referencia: req.params.referencia });
+});
+
+var URLEMPRESA = "https://impo-piero.rossi.cl/login";
+var logText = "";
+var webdriver = require("selenium-webdriver"),
+  By = webdriver.By,
+  until = webdriver.until;
+const screen = {
+  width: 1360,
+  height: 700,
+};
+var { parseFromString } = require("dom-parser");
+
+var firefox = require("selenium-webdriver/firefox");
+var driver;
+
 const procesaAgenda = async (req, res, taskdata) => {
   console.log("Inicio Ejecución Programada RPA Rossi");
+  driver = new webdriver.Builder()
+    .forBrowser("firefox")
+    .setFirefoxOptions(new firefox.Options().set("acceptInsecureCerts", true))
+    .build();
+
+  await driver.get(URLEMPRESA);
+  await driver.manage().window().setRect(screen);
+
+  var rut = await driver.wait(
+    until.elementLocated(By.xpath('//*[@id="username"]')),
+    20000
+  );
+  await rut.sendKeys("19686132-7" + webdriver.Key.TAB);
+
+  var pwd = await driver.wait(
+    until.elementLocated(By.xpath('//*[@id="password"]')),
+    20000
+  );
+  await pwd.sendKeys("JBSrossi1" + webdriver.Key.TAB);
+
+  var btn = await driver.wait(
+    until.elementLocated(By.xpath('//*[@id="form"]/footer/button')),
+    20000
+  );
+
+  await btn.click();
+  await driver.sleep(2000);
+
+  await procesaDetalles(taskdata.referencia);
+
   res.send({
     error: false,
     message: "Inicio Ejecución Programada RPA Rossi",
   });
+};
+
+const procesaDetalles = async (referencia) => {
+  console.log("Inicio Ejecución Programada RPA Rossi", referencia);
+
+  var numref = null;
+  while (true) {
+    try {
+      await numref.sendKeys(referencia + webdriver.Key.TAB);
+      break;
+    } catch (e) {
+      numref = await driver.wait(
+        until.elementLocated(
+          By.xpath('//*[@id="exportar"]/fieldset/div[10]/input')
+        ),
+        20000
+      );
+    }
+  }
+
+  var btn = await driver.wait(
+    until.elementLocated(
+      By.xpath('//*[@id="exportar"]/fieldset/div[18]/button')
+    ),
+    20000
+  );
+  await btn.click();
+
+  var subtabla = await driver.wait(
+    until.elementsLocated(By.xpath('//*[@id="tabla"]')),
+    20000
+  );
+
+  var sublinea = 0;
+  while (sublinea < subtabla.length) {
+    var sublineaHTML = await subtabla[sublinea].getAttribute("innerHTML");
+    var dom = parseFromString(sublineaHTML);
+    var columnas = dom.getElementsByTagName("td");
+
+    var nroDespacho = columnas[0].textContent.trim();
+    var tipoTranporte = columnas[0].innerHTML.includes("fa-truck")
+      ? "Terrestre"
+      : "Maritimo";
+    var tipoOperacion = columnas[1].textContent.trim();
+    var fechaETA = columnas[2].textContent.trim();
+    var proveedor = columnas[3].textContent
+      .trim()
+      .split("Regimen Importación")[0];
+    var regimen = columnas[3].textContent
+      .trim()
+      .split("Regimen Importación")[1];
+
+    var refCliente = columnas[4].textContent.trim().split("Impuesto DI")[0];
+    var impuestoDI = columnas[4].textContent
+      .trim()
+      .split("Impuesto DI")[1]
+      .replace("USD", "")
+      .replace(".", "")
+      .trim();
+
+    var puertoEmbarque = columnas[5].textContent.trim().split("País Origen")[0];
+    var paisEmbarque = columnas[5].textContent.trim().split("País Origen")[1];
+
+    var aduana = columnas[6].textContent.trim().split("Puerto Descarga")[0];
+    var puertoDescarga = columnas[6].textContent
+      .trim()
+      .split("Puerto Descarga")[1];
+
+    sublinea++;
+  }
+
+  console.log("Esperando 10 segundos");
+  await driver.sleep(10000);
 };
 
 exports.RpaRossiRoutes = router;
