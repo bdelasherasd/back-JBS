7ccee4e1395bb64b71aa5b1063e3c6bd93728b07
osboxes.org <bdelasheras@outlook.com>
2025-04-22 19:20:15 -0400

    update-detlle-ok

--- routes/importaciones.js
+++ routes/importaciones.js
@@ -37,6 +37,40 @@ router.get("/listImportaciones", cors(), async function (req, res) {
   }
 });
 
+router.get("/getImportacion/:id", cors(), async function (req, res) {
+  showLog(req, res);
+  try {
+    let data = await imp_importacion_archivo.findOne({
+      where: {
+        idImportacion: req.params.id,
+      },
+    });
+    //res.status(200).json({data})
+    res.send(data);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+router.get("/listCodigos/:id", cors(), async function (req, res) {
+  showLog(req, res);
+
+  let sql = "";
+  sql += "select sku , producto ";
+  sql += "from imp_skus join ";
+  sql += "imp_importacions on imp_skus.proveedor=imp_importacions.proveedor ";
+  sql += "where imp_importacions.idImportacion=" + req.params.id;
+  sql += "order by sku ";
+  try {
+    let data = await sequelize.query(sql);
+    data = data[0];
+    //res.status(200).json({data})
+    res.send(data);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
 router.get("/listGastosAgencia/:id", cors(), async function (req, res) {
   showLog(req, res);
   let sql = "";
@@ -65,6 +99,49 @@ router.get("/listAdicionales/:id", cors(), async function (req, res) {
     res.send(data);
   } catch (error) {
     console.log(error.message);
+  }
+});
+
+router.post("/updateDetalles", cors(), async function (req, res) {
+  showLog(req, res);
+
+  let datos = {
+    idImportacion: req.sanitize(req.body.idImportacion),
+    index: req.sanitize(req.body.index),
+    codigo: req.sanitize(req.body.codigo),
+    cantidad: req.sanitize(req.body.cantidad),
+    valor: req.sanitize(req.body.valor),
+  };
+
+  try {
+    let resultImp = await imp_importacion_archivo.findOne({
+      where: {
+        idImportacion: req.body.idImportacion,
+      },
+    });
+
+    let det = JSON.parse(resultImp.detalles);
+    let item = {
+      codigo: req.body.codigo,
+      cantidad: req.body.cantidad,
+      valor: req.body.valor,
+      descripcion: "",
+      codigoInvalido: false,
+    };
+    det[req.body.index] = item;
+    let detalles = JSON.stringify(det);
+
+    let datos = {
+      detalles: detalles,
+    };
+
+    let result = await imp_importacion_archivo.update(datos, {
+      where: { idImportacion: req.sanitize(req.body.idImportacion) },
+    });
+    result["error"] = false;
+    res.send(result);
+  } catch (error) {
+    console.log({ username: false, error: error.message });
   }
 });
 
