77f725c927fcd132df0b6f67a78a568fd865e916
osboxes.org <bdelasheras@outlook.com>
2025-06-13 10:56:29 -0400

    invoice-en-detalle-reformateo-datos2

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   const origin = req.get("Origin");
   if (process.env.CERRAR_APIS === "SI") {
     if (!origin || !allowedOrigins.includes(origin)) {
-      return res.status(403).send("Origen no permitido");
+      return res.status(403).send("Origen no permitido.... Acceso denegado.");
     }
   }
   next();
