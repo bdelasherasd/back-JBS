bb0fb748d1f02f5461363ba6a5a7394456aa1ea7
osboxes.org <bdelasheras@outlook.com>
2025-06-18 19:36:55 -0400

    valida encabezados excel skus

--- routes/cargaSku copy.js
+++ routes/cargaSku copy.js
@@ -47,7 +47,9 @@ router.post(
         .status(200)
         .send({ message: "Archivo procesado correctamente", ok: true });
     } catch (error) {
-      res.status(500).send({ error: error.message, ok: false });
+      res
+        .status(500)
+        .send({ error: "Error al procesar el archivo", ok: false });
     }
   }
 );
@@ -57,52 +59,50 @@ async function processExcel(filePath) {
   const sheetName = workbook.SheetNames[0];
   const worksheet = workbook.Sheets[sheetName];
 
-  //const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+  const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
   const jsonData2 = XLSX.utils.sheet_to_json(worksheet);
+
+  // Eliminar la primera fila (encabezados)
+  jsonData.shift();
 
   let rowCount = 0;
   // Guardar los datos en la base de datos
-  for (const row of jsonData2) {
+  for (const row of jsonData) {
     rowCount++;
-
-    let erroresEncabezado = "";
-    if (rowCount === 1) {
-      erroresEncabezado = await validaEncabezados(row);
-      if (erroresEncabezado !== "") {
-        console.error("Error en los encabezados:", erroresEncabezado);
-        throw new Error(`Error en los encabezados: ${erroresEncabezado}`);
-      }
-    }
-
-    if (!row.SKU) {
+    if (!row[0]) {
       break; // Detener el procesamiento si la primera celda está vacía}
+    }
+    if (row.length == 8) {
+      row.push(""); // Agregar un campo vacío si la fila tiene 8 columnas
+    }
+    if (row.length < 9) {
+      console.log("Fila incompleta, se omite:", row);
+      continue; // Omitir filas incompletas
     }
     //console.log(rowCount, row);
 
-    let sku = row.SKU.toString().trim();
-
     try {
       const skuData = {
-        sku: sku,
-        producto: row.Producto,
-        proteina: row.PROTEINA,
-        origen: row.ORIGEN,
-        marca: row.MARCA,
-        proveedor: row.PROVEEDOR,
-        estado: row.ESTADO,
-        calidad: row.CALIDAD,
-        tipo: row.TIPO,
+        sku: row[0].toString(),
+        producto: row[1].toString(),
+        proteina: row[2].toString(),
+        origen: row[3].toString(),
+        marca: row[4].toString(),
+        proveedor: row[5].toString(),
+        estado: row[6].toString(),
+        calidad: row[7] ? row[7].toString() : "",
+        tipo: row[8] ? row[8].toString() : "",
       };
       const existe = await imp_sku.findOne({
         where: {
-          sku: sku,
+          sku: row[0].toString(),
         },
       });
       if (existe) {
         //console.log(skuData.sku);
         await imp_sku.update(skuData, {
           where: {
-            sku: sku,
+            sku: row[0].toString(),
           },
         });
       } else {
@@ -114,39 +114,6 @@ async function processExcel(filePath) {
     }
   }
   console.log("Datos guardados en la base de datos");
-}
-
-async function validaEncabezados(row) {
-  let errores = [];
-  if (!row.SKU) {
-    errores.push("Falta el encabezado SKU");
-  }
-  if (!row.Producto) {
-    errores.push("Falta el encabezado Producto");
-  }
-  if (!row.PROTEINA) {
-    errores.push("Falta el encabezado PROTEINA");
-  }
-  if (!row.ORIGEN) {
-    errores.push("Falta el encabezado ORIGEN");
-  }
-  if (!row.MARCA) {
-    errores.push("Falta el encabezado MARCA");
-  }
-  if (!row.PROVEEDOR) {
-    errores.push("Falta el encabezado PROVEEDOR");
-  }
-  if (!row.ESTADO) {
-    errores.push("Falta el encabezado ESTADO");
-  }
-  if (!row.CALIDAD) {
-    errores.push("Falta el encabezado CALIDAD");
-  }
-  if (!row.TIPO) {
-    errores.push("Falta el encabezado TIPO");
-  }
-
-  return errores.join(", ");
 }
 
 module.exports = router;
